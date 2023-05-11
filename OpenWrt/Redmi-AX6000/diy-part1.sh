c67d4f1fc42a51f7fec462202cd52c9145d3de0e
lyz2202 <133206240+lyz2202@users.noreply.github.com>
2023-05-11 16:33:46 +0800

    Update diy-part1.sh

--- OpenWrt/Redmi-AX6000/diy-part1.sh
+++ OpenWrt/Redmi-AX6000/diy-part1.sh
@@ -12,6 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git;master' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
